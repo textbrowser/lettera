10b969b8c58c7302816a80f6f619c4c0f7ede5de
Monster <textbrowser@gmail.com>
2018-09-03 17:06:46 -0400

    New files.

---
+++ Lettera/app/src/main/java/org/purple/lettera/Lettera.java
@@ -0,0 +1,13 @@
+package org.purple.lettera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lettera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lettera);
+    }
+}
