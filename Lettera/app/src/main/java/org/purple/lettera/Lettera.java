adb84cd423620b2f9a60fc5d98fef80511b1a796
textbrowser@gmail.com <textbrowser@gmail.com>
2019-11-25 18:21:28 -0500

    Highlight on tap.

--- Lettera/app/src/main/java/org/purple/lettera/Lettera.java
+++ Lettera/app/src/main/java/org/purple/lettera/Lettera.java
@@ -843,11 +843,6 @@ public class Lettera extends AppCompatActivity
 		@Override
 		public void onClick(View view, int position)
 		{
-		}
-
-		@Override
-		public void onLongClick(View view, int position)
-		{
 		    m_artificial_button.performClick();
 
 		    if(m_layout_manager.
@@ -856,10 +851,16 @@ public class Lettera extends AppCompatActivity
 			    (m_selected_position).setBackgroundColor
 			    (background_color());
 
-		    m_letter_dialog.show
-			(email_account(), selected_folder_name(), position);
 		    m_selected_position = position;
 		    view.setBackgroundColor(SELECTION_COLOR);
+		}
+
+		@Override
+		public void onLongClick(View view, int position)
+		{
+		    onClick(view, position);
+		    m_letter_dialog.show
+			(email_account(), selected_folder_name(), position);
 		}
 	    }));
 	m_recycler.addOnScrollListener
